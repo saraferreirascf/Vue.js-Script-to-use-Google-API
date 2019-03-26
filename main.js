283996b6387fbea08fc0211d38217a64724881f7
Sara Ferreira <44060413+saraferreirascf@users.noreply.github.com>
2019-03-26 17:24:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import './lib/css'
-import './lib/script'
-import './lib/global'
-import './lib/custom.css'
-
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import EventBus from './lib/eventBus.js'
-import axios from 'axios'
-import vmodal from 'vue-js-modal'
-import VueSimpleSpinner from './widgets/Spinner'
-//import GoogleAuth from 'vue-google-authenticator'
 import VueGoogleApi from 'vue-google-api'
 
-Vue.prototype.$bus = EventBus
-Vue.prototype.$http = axios
-
-if (window.webpackHotUpdate) {
-  console.log('This is dev')
-  axios.defaults.baseURL = 'http://localhost:3000/'
-} else {
-  axios.defaults.baseURL = window.location.protocol + '//' + window.location.host
-}
-
-axios.credentials = false
-
 const config = {
-  apiKey: 'AIzaSyAGB7160bJr9UqAsIKs6h8h9jH9HSQlbf0',
-  clientId: '298218985189-k7niq94udl860m43bfiaqb3gb3l5ntfp.apps.googleusercontent.com',
+  apiKey: YOUR_API_KEY,
+  clientId: YOUR_CLIENT_ID,
   scope: 'profile https://www.googleapis.com/auth/drive ',
   cookiepolicy: 'single_host_origin'
-  //discoveryDocs: 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
 }
 
 Vue.use(VueGoogleApi, config,vmodal, {
@@ -40,7 +15,7 @@ Vue.use(VueGoogleApi, config,vmodal, {
   dynamic: true
 })
 
-//Vue.googleAuth().load()
+
 
 /* eslint-disable no-new */
 export const globalvm = new Vue({
